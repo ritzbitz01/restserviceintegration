83bedc5fdc15441d1f71bf3af354c22131e82036
Ritz <ryan.ritz@pearson.com>
2017-02-24 11:31:28 -0700

    Initial commit

---
+++ src/main/java/com/rbp/simplespring/restserviceintegration/App.java
@@ -0,0 +1,13 @@
+package com.rbp.simplespring.restserviceintegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
